0002f73e9f071f300e9fd44c0676256f60e7a1d6
crizen <crizen@CZ-kmki-N1>
2018-11-29 10:08:34 +0900

    mission1 업로드

---
+++ mission1/src/main/java/com/crizen/mission/mission1Cont.java
@@ -0,0 +1,53 @@
+package com.crizen.mission;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class mission1Cont {
+
+  public mission1Cont() {
+    System.out.println("---mission1Cont() 객체 생성");
+  }//기본 생성자
+  
+  //결과확인 http://localhost:8080/mission1/watch.do
+  
+  @RequestMapping(value="/watch.do", method=RequestMethod.GET)
+  public ModelAndView watchForm() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("mission1/watchForm");
+    return mav;
+  }//watchForm() end
+  
+  @RequestMapping(value = "/timer.do", method = RequestMethod.GET)
+  public void watchView(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    try {
+      resp.setContentType("text/plain; charset=UTF-8");
+      PrintWriter out = resp.getWriter(); //출력객체
+      
+      //시간 구하기
+      //출력결과 : 현재 시간은  YYYY-MM-dd HH24:mm:ss  입니다.
+      Calendar cal = Calendar.getInstance();
+      Date date = cal.getTime();
+      String today = (new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(date));
+
+      out.println(today);
+      out.flush();
+      out.close(); //자원반납
+    } catch (Exception e) {
+      System.out.println("실패 : " + e);
+    }
+  }//watchView() end
+
+}//class end
---
+++ mission1/src/main/webapp/mission1/watchForm.jsp
@@ -0,0 +1,52 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>mission1/watch</title>
+<style>
+.time {
+  background-color: #000000;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 20px;
+  text-align: center;
+  border: 0;
+}
+</style>
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+</head>
+<!-- <body onload="showtime()" onunload="killtime()"> -->
+  <body>
+  <form name="myform">
+    <div id="clock" class="time"></div>
+    <!-- <input type="text" id="clock" name="clock" size="50" class="time" readonly> -->
+  </form>
+  
+<script src="js/jquery.js"></script>
+<script>
+  //ajax 객체의 기본값 지정
+  $.ajaxSetup({datatype:"text"});
+  
+  $(document).ready(function() {
+    setInterval(function() {
+      showTime();
+      },1000);
+  }); //ready() end
+
+  function showTime() {
+    //alert("test");
+    $.get("timer.do", watchView);
+  }//showTime() end
+
+  function watchView(result) {
+    //alert(result);
+    var str = "현재시간은 ";
+    str += result + "입니다";
+    $("#clock").empty();
+    $("#clock").text(str);
+  }//watchView() end
+</script>
+
+</body>
+</html>
